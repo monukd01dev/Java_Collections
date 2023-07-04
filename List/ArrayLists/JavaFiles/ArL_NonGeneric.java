bf225d83140da485ffbf556bd8e2c3791d82047b
MonuKD47 <monukd01dev@gmail.com>
2023-07-05 02:53:30 +0530

    ArrayList is going on

---
+++ List/ArrayLists/JavaFiles/ArL_NonGeneric.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArL_NonGeneric{
+	
+	
+	public static void main(String[] args){
+		
+		// Represeting Non Generic 
+		List arrlist = new ArrayList();
+		
+		// using add method to add 
+		arrlist.add("Monu Kumar");
+		arrlist.add(19);
+		arrlist.add(19.5f);
+		arrlist.add(1945l);
+		arrlist.add('J');
+		
+		System.out.println(arrlist);
+	}
+	
+}
